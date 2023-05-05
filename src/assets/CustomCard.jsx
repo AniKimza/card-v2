99efa5f43e577ec91153e620e9a0ead923465e37
AniKimza <ani.kimza@gmail.com>
2023-05-05 23:19:21 +0200

    Setup for animation

--- src/assets/CustomCard.jsx
+++ src/assets/CustomCard.jsx
@@ -1,8 +1,5 @@
-import Card from '@mui/material/Card';
 import { motion } from "framer-motion";
-// import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Stack, TextField } from '@mui/material';
+import { Stack, Card, Typography, Box } from '@mui/material';
 import { useState } from 'react';
 import { SearchBar } from './SearchBar';
 
@@ -33,9 +30,26 @@ export const CustomCard = () => {
                                 {title}
                             </Typography>
                             <Stack direction={'row'} alignItems={'center'} spacing={5} sx={{ flex: 1 }}>
-                                <Typography variant='h6' fontFamily={'Barlow, sans-serif'}>
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            <Box sx={{ height: '300px', overflow: 'scroll', '&::-webkit-scrollbar': { width: '0em' }, '&::-webkit-scrollbar-thumb': { backgroundColor: '#1E1E1E' }}}>
+                                <Typography variant='h5' fontFamily={'Josefin Sans, sans-serif'}>
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                                 </Typography>
+                                <Typography variant='h5' fontFamily={'Josefin Sans, sans-serif'}>
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </Typography>
+                                <Typography variant='h5' fontFamily={'Josefin Sans, sans-serif'}>
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </Typography>
+                                <Typography variant='h5' fontFamily={'Josefin Sans, sans-serif'}>
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </Typography>
+                                <Typography variant='h5' fontFamily={'Josefin Sans, sans-serif'}>
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </Typography>
+                                <Typography variant='h5' fontFamily={'Josefin Sans, sans-serif'}>
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </Typography>
+                                </Box>
                                 <img
                                     style={{ width: '35%', height: '70%', borderRadius: 10 }}
                                     src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
