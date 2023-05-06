3548356f53dc3470524bd834091f5e98bbdb6670
AniKimza <ani.kimza@gmail.com>
2023-05-06 02:05:40 +0200

    Replace Stack with Grid

--- src/assets/CustomCard.jsx
+++ src/assets/CustomCard.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Stack, Card, Typography, Box } from '@mui/material';
+import { Stack, Card, Typography, Box, Grid } from '@mui/material';
 import { useState } from 'react';
 import { SearchBar } from './SearchBar';
 import { AnimationTxt } from "./AnimationTxt";
@@ -13,11 +13,11 @@ export const CustomCard = () => {
         setTitle(e.target.value)
     }
 
-const typo = ['It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.']
+    const typo = ['It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.']
 
 
     return (
@@ -33,25 +33,29 @@ const typo = ['It was popularised in the 1960s with the release of Letraset shee
             >
                 <Stack height={'100vh'} alignItems={'center'} justifyContent={'center'} spacing={4}>
                     <Card sx={{ width: '50%', height: '50%', borderRadius: 7, padding: 4 }}>
-                        <Stack height={'100%'} alignItems={'center'} justifyContent={'flex-start'}>
-                            <Typography variant='h3' fontFamily={'Bruno Ace SC'} sx={{ textAlign: 'center', height: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                {title}
-                            </Typography>
-                            <Stack direction={'row'} alignItems={'center'} spacing={5} sx={{ flex: 1 }}>
-                            <Box sx={{ height: '300px', overflow: 'scroll', '&::-webkit-scrollbar': { width: '0em' }, '&::-webkit-scrollbar-thumb': { backgroundColor: '#1E1E1E' }}}>
-                               {typo.map((item, index) => {
-                                return <AnimationTxt key={index} index={index}>
-                                 <Typography variant='h5' fontFamily={'Josefin Sans, sans-serif'}>{item}</Typography>
-                                 </AnimationTxt>
-                               })}
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <Typography variant='h3' fontFamily={'Bruno Ace SC'} sx={{ textAlign: 'center', height: '60px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                    {title}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={7}>
+                                <Box sx={{ height: '0%', overflow: 'scroll', '&::-webkit-scrollbar': { width: 0 }, '&::-webkit-scrollbar-thumb': { backgroundColor: '#1E1E1E' } }}>
+                                    {typo.map((item, index) => {
+                                        return <AnimationTxt key={index} index={index}>
+                                            <Typography variant='h5' fontFamily={'Josefin Sans, sans-serif'}>{item}</Typography>
+                                        </AnimationTxt>
+                                    })}
                                 </Box>
+                            </Grid>
+                            <Grid item xs={5}>
                                 <img
-                                    style={{ width: '35%', height: '70%', borderRadius: 10 }}
+                                    style={{ width: '100%', borderRadius: 10 }}
                                     src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
                                     alt="new"
                                 />
-                            </Stack>
-                        </Stack>
+                            </Grid>
+                        </Grid>
                     </Card>
                     <SearchBar handle={handleTitle} title={title} />
                 </Stack>
